66fd3fc801215936073d6e7fbca6de7ac6d3b26c
tahirshaikhs <114974768+tahirshaikhs@users.noreply.github.com>
2026-01-31 01:20:31 +0530

    Update instagram.py

--- app/instagram.py
+++ app/instagram.py
@@ -1,14 +1,13 @@
-# app/instagram.py
 import os
 import requests
 
-SESSIONID = os.environ.get("19c1068529a-41da83")
+SESSIONID = os.getenv("INSTAGRAM_SESSIONID")
 
 if not SESSIONID:
     raise RuntimeError("INSTAGRAM_SESSIONID is missing")
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
     "X-IG-App-ID": "936619743392459",
     "Cookie": f"sessionid={SESSIONID}",
 }
@@ -17,8 +16,11 @@ def fetch_posts(keyword):
     url = "https://www.instagram.com/api/v1/web/search/topsearch/"
     params = {"query": keyword}
 
-    r = requests.get(url, headers=HEADERS, params=params)
+    r = requests.get(url, headers=HEADERS, params=params, timeout=15)
+
     if r.status_code != 200:
-        raise RuntimeError("Instagram blocked request")
+        raise RuntimeError(
+            f"Instagram blocked request ({r.status_code})"
+        )
 
     return r.json()
