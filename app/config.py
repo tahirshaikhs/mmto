dfe22e72727621441507c58b4b5347503f3e8d20
tahirshaikhs <114974768+tahirshaikhs@users.noreply.github.com>
2026-01-31 00:31:44 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,12 @@
-import os
+# app/config.py
 
-BOT_TOKEN = os.environ["8099434896:AAFwFQk1qChv_Jxb2VhvXR8u8_E4TIs1_og"]
-CHANNEL = os.environ["@desidekhoo"]
-DATABASE_URL = os.environ["postgresql://tahir:<ENTER-SQL-USER-PASSWORD>@autopost-db-21131.j77.aws-ap-south-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"]
-INSTAGRAM_USERNAME = os.environ.get("INSTAGRAM_USERNAME")  # optional
+BOT_TOKEN = "8099434896:AAFwFQk1qChv_Jxb2VhvXR8u8_E4TIs1_og"
+CHANNEL = "@desidekhoo"
+
+DATABASE_URL = (
+    "postgresql://tahir:<ENTER-SQL-USER-PASSWORD>"
+    "@autopost-db-21131.j77.aws-ap-south-1.cockroachlabs.cloud:26257/defaultdb"
+    "?sslmode=verify-full"
+)
+
+INSTAGRAM_USERNAME = None
